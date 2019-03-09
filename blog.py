2d8029a5a0f2629423bb03cb06ac23352de42899
canonka <canonka444@hotmail.com>
2019-03-09 20:37:23 +0300

    Bazı tamplateler eklendi, kayıt formu oluşturuldu.

--- blog.py
+++ blog.py
@@ -1,15 +1,53 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
+
+# kullanıcı kayıt formu
+class RegisterForm(Form):
+    name = StringField("İsim Soyisim",validators=[validators.length(min = 4, max = 25)])
+    username = StringField("Kullanıcı Adı",validators=[validators.length(min = 5, max = 35)])
+    email = StringField("Email Adresi",validators=[validators.Email(message="Lütfen Geçerli Bir Email Adresi Girin.")])
+    password = PasswordField("Parola:",validators=[
+        validators.DataRequired(message="Lütfen bir parola belirleyin."),
+        validators.EqualTo(fieldname="confirm",message="Parolanız Uyuşmuyor...")
+    ])
+    confirm = PasswordField("Parola Doğrula")
 
 
 app = Flask(__name__)
 
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "ybblog"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+
+mysql = MySQL(app)
+
 @app.route("/")
 def index():
-    article = dict()
-    article["title"] = "Ana Sayfa"
-    article["body"] = "Deneme 123"
-    article["author"] = "Batuhan ŞENER"
-    return render_template("index.html",article = article)
+    articles = [
+        {"id":1,"title":"Deneme1","content":"Deneme1 içerik"},
+        {"id":2,"title":"Deneme2","content":"Deneme2 içerik"},
+        {"id":3,"title":"Deneme3","content":"Deneme3 içerik"}
+    ]
+    return render_template("index.html",articles = articles)
+
+#hakkında kısmını oluşturduk.
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+#kayıt olma register
+@app.route("/register",methods = ["GET","POST"])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("register.html",form = form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
