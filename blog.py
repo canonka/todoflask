b0e81adb5f59a06080d3a1ac3d7d3fb4c2d297e8
canonka <canonka444@hotmail.com>
2019-03-10 14:15:21 +0300

    Makaleler için Güncelle ve sil özelliği eklendi. Arama butonu oluşturuldu. Progenin son hali makalelerdeki <blog.py'im> adlı makaleye eklendi

--- blog.py
+++ blog.py
@@ -2,6 +2,19 @@ from flask import Flask,render_template,flash,redirect,url_for,session,logging,r
 from flask_mysqldb import MySQL
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
+from functools import wraps
+
+#Kullanıcı giriş DECORATOR'ı
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            flash("Bu sayfayı görüntülemek için lütfen giriş yapın.","danger")
+            return redirect(url_for("login"))
+
+    return decorated_function
 
 # kullanıcı kayıt formu
 class RegisterForm(Form):
@@ -29,6 +42,7 @@ app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
+# indexe yönlendirme yaptık.
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -37,6 +51,33 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+#Makale Sayfası
+@app.route("/articles")
+def articles():
+    cursor = mysql.connection.cursor()
+    sorgu = "Select * From articles"
+    result = cursor.execute(sorgu)
+    if result > 0:
+        articles = cursor.fetchall()
+        return render_template("articles.html",articles = articles)
+    else:
+        return render_template("articles.html")
+
+#Kontrol Paneli oluşturuldu.
+@app.route("/dashboard")
+#login oldu mu olmadımı kontol edip öyle sayfaya gidiyor.
+@login_required
+def dashboard():
+    cursor = mysql.connection.cursor()
+    sorgu = "Select * From articles where author = %s"
+    #database sorgumuzu gerçekleştirdik.
+    result = cursor.execute(sorgu,(session["username"],))
+    if result > 0:
+        articles = cursor.fetchall()
+        return render_template("dashboard.html",articles = articles)
+    else:
+        return render_template("dashboard.html")
 
 #kayıt olma register
 @app.route("/register",methods = ["GET","POST"])
@@ -89,12 +130,117 @@ def login():
             return redirect(url_for("login"))
     return render_template("login.html",form=form)
 
+#Makale Detay sayfası
+@app.route("/article/<string:id>")
+def article(id):
+    cursor = mysql.connection.cursor()
+
+    sorgu = "Select * From articles where id = %s"
+
+    result = cursor.execute(sorgu,(id,))
+
+    if result > 0:
+        article = cursor.fetchone()
+        return render_template("article.html",article = article)
+    else:
+        return render_template("article.html")
+
+
 #logout çıkış işlemi
 @app.route("/logout")
 def logout():
     session.clear()
     flash("Başarıyla çıkış yaptınız...","success")
     return redirect(url_for("index"))
+
+#Makale ekleme
+@app.route("/addarticle",methods = ["GET","POST"])
+def addarticle():
+    form = ArticleForm(request.form)
+    if request.method == "POST" and form.validate():
+        title = form.title.data
+        content = form.content.data
+        
+        cursor = mysql.connection.cursor()
+
+        sorgu = "Insert into articles(title,author,content) Values(%s,%s,%s)"
+        cursor.execute(sorgu,(title,session["username"],content))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash("Makale başarıyla eklendi...","success")
+        return redirect(url_for("dashboard"))
+
+    return render_template("addarticle.html",form = form)
+
+#Makale Silme
+@app.route("/delete/<string:id>")
+#login_required olduğu için önce giriş yapmamızı isteyecek
+@login_required
+def delete(id):
+    cursor = mysql.connection.cursor()
+    sorgu = "Select * from articles where author = %s and id = %s"
+    result = cursor.execute(sorgu,(session["username"],id))
+    if result > 0:
+        sorgu2 = "Delete from articles where id = %s"
+        cursor.execute(sorgu2,(id,))
+        mysql.connection.commit()
+        return redirect(url_for("dashboard"))
+    else:
+        flash("Bu işleme yetkiniz yoktur ","danger")
+        return redirect(url_for("index"))
+
+#Makele güncelleme
+@app.route("/edit/<string:id>",methods = ["GET","POST"])
+@login_required
+def update(id):
+    if request.method == "GET":
+        cursor = mysql.connection.cursor()
+        sorgu = "Select * from articles where id = %s and author = %s"
+        result = cursor.execute(sorgu,(id,session["username"]))
+        if result == 0:
+            flash("Böyle bir makale yok ya da bu işleme yetkiniz yok","danger")
+            return redirect(url_for("index"))
+        else:
+            article = cursor.fetchone()
+            form = ArticleForm()
+            form.title.data = article["title"]
+            form.content.data = article["content"]
+            return render_template("update.html",form = form)
+    else:
+        #Post REQUEST kısmı
+        form = ArticleForm(request.form)
+        newTitle = form.title.data
+        newContent = form.content.data
+        sorgu2 = "Update articles Set title = %s, content = %s where id = %s"
+        cursor = mysql.connection.cursor()
+        cursor.execute(sorgu2,(newTitle,newContent,id))
+        #commit veritabanında değişiklik yaptığımızda kullanıyoruz.
+        mysql.connection.commit()
+        flash("Makale başarıyla güncellendi...","success")
+        return redirect(url_for("dashboard"))
+
+#Makale Formu
+class ArticleForm(Form):
+    title = StringField("Makale Başlığı", validators=[validators.Length(min = 5, max=75)])
+    content = TextAreaField("Makale İçeriği",validators=[validators.length(min = 10)])
+
+#Arama URL
+@app.route("/search",methods = ["GET","POST"])
+def search():
+    if request.method == "GET":
+        return redirect(url_for("index"))
+    else:
+        keyword = request.form.get("keyword")
+        cursor = mysql.connection.cursor()
+        sorgu = "Select * from articles where title like '%"+keyword+"%'"
+        result = cursor.execute(sorgu)
+        if result == 0:
+            flash("Aranan kelimeye uygun makale bulunamadı!","warning")
+            return redirect(url_for("articles"))
+        else:
+            articles = cursor.fetchall()
+            return render_template("articles.html",articles = articles)
 
 if __name__ == "__main__":
     app.run(debug=True)
